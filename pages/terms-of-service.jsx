0c23e1249c5140476b99fcb881e265159b35cdc4
Irshad Ansari <irshadjsr21@gmail.com>
2021-01-24 22:01:47 +0530

    📦 NEW: Terms of Service

---
+++ pages/terms-of-service.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from '../src/components/Header';
+import Terms from '../src/components/Terms/index';
+
+const About = () => (
+  <div>
+    <Header />
+    <Terms />
+  </div>
+);
+export default About;
