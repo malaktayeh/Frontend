da371658d2709b7cfe2a664809db51b807e5f260
himanshu16singh <hsnice16@gmail.com>
2021-04-22 09:29:43 +0530

    feat: add feature in issue #329

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 import styles from '../../scss/navbar.module.scss';
 import Burger from './Burger';
@@ -6,9 +7,13 @@ import Burger from './Burger';
 const Navbar = () => {
   return (
     <div className={styles.stylenavbar}>
-      <div className={styles.logo}>
-      <img className= {styles['header-logo']} src="/logo/codetrophs.png" alt="" />
-      </div>
+      <Link href='/feed'>
+        <a>
+          <div className={styles.logo}>
+            <img className= {styles['header-logo']} src="/logo/codetrophs.png" alt="" />
+          </div>
+        </a>
+      </Link>
       <Burger />
     </div>
   )
